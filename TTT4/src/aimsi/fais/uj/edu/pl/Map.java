cdf24164824048a5dfe534dec31c1afd42bad599
Konrad Welc <konrad1331@gmail.com>
2014-11-03 21:18:20 +0100

    Map.java ->Victory and draw check

--- TTT4/src/aimsi/fais/uj/edu/pl/Map.java
+++ TTT4/src/aimsi/fais/uj/edu/pl/Map.java
@@ -12,12 +12,15 @@ package aimsi.fais.uj.edu.pl;
 public class Map {
 
     static FieldType[][][] fields = new FieldType[4][4][4];
+    static int moves;
 
     Map() {
         restart();
+        
     }
 
     public void restart() {
+        moves=0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 for (int k = 0; k < 4; k++) {
@@ -58,7 +61,7 @@ public class Map {
         //VICTORY_X, VICTORY_O, DRAW,
         //TODO Check Victory
         
-        if (victoryCheck(x, y, z)) {
+        if (victoryCheck(x, y, z, sign)) {
             if (FieldType.O == sign) {
                 return MoveResult.VICTORY_O;
             } else {
@@ -73,12 +76,70 @@ public class Map {
         return MoveResult.SUCCESS;
     }
 
-    private boolean victoryCheck(int x, int y, int z) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private boolean victoryCheck(int x, int y, int z, FieldType sign) {
+        //counters
+        int c1 = 0, c2 = 0, c3 = 0, c4 = 0;
+        
+        // linear check
+        for(int i=0;i<4;i++){
+            if(fields[i][y][z] == sign) c1++;
+            if(fields[x][i][z] == sign) c2++;
+            if(fields[x][y][i] == sign) c3++;
+        }
+        
+        if( c1==4 || c2==4 || c3==4 )return true;
+        else{ c1=0; c2=0; c3=0; }
+        
+        //slant check - topleft
+        for(int i=0; i<4; i++){
+            if(fields[i][i][z] == sign) c1++;
+            if(fields[i][y][i] == sign) c2++;
+            if(fields[x][i][i] == sign) c3++;
+            if(fields[i][i][i] == sign) c4++;
+        }
+        
+        if( c1==4 || c2==4 || c3==4 || c4==4 )return true;
+        else{ c1=0; c2=0; c3=0; c4=0; }
+        
+        //slant check - topright
+        for(int i=0; i<4; i++){
+            if(fields[4-i][i][z] == sign) c1++;
+            if(fields[4-i][y][i] == sign) c2++;
+            if(fields[4-i][i][i] == sign) c4++;
+        }
+        
+        if( c1==4 || c2==4 || c4==4 )return true;
+        else{ c1=0; c2=0; c4=0; }
+        
+        //slant check - bottomright
+        for(int i=0; i<4; i++){
+            if(fields[i][4-i][z] == sign) c1++;
+            if(fields[x][4-i][i] == sign) c3++;
+            if(fields[i][4-i][i] == sign) c4++;
+        }
+        
+        if( c1==4 || c3==4 || c4==4 )return true;
+        else{ c1=0; c3=0; c4=0; }
+        
+        //slant check - bottomleft
+        for(int i=0; i<4; i++){
+            if(fields[4-i][y][i] == sign) c2++;
+            if(fields[x][4-i][i] == sign) c3++;
+            if(fields[4-i][4-i][i] == sign) c4++;
+        }
+        
+        if( c2==4 || c3==4 || c4==4 )return true;
+        else{ c2=0; c3=0; c4=0; }
+        
+        
+        return false;
     }
 
     private boolean drawCheck() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+           moves++;
+           if(moves == 64) return true;
+           
+           return false;
     }
 
 }
