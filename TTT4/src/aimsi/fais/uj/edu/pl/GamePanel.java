7cf644253279cb4488786a9eac7406d156372c29
Bartosz Bereza <bbplen@gmail.com>
2014-11-04 16:57:57 +0100

    Grafiki O i X plus niewielkie zmiany w GamePanelu pod kątem wyświetlania.

--- TTT4/src/aimsi/fais/uj/edu/pl/GamePanel.java
+++ TTT4/src/aimsi/fais/uj/edu/pl/GamePanel.java
@@ -3,21 +3,36 @@ package aimsi.fais.uj.edu.pl;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import java.io.File;
+import java.io.IOException;
+
 import javax.swing.JPanel;
+import javax.imageio.ImageIO;
 
 /**
  *
  * @author Bartosz Bereza
  */
 class GamePanel extends JPanel {
+
     int z = 0;
     private final GameWindow outer;
+    private BufferedImage O;
+    private BufferedImage X;
 
     GamePanel(int z, final GameWindow outer) {
         this.outer = outer;
         this.setSize(new Dimension(400, 400));
         this.setBackground(new Color(0, 0, z * 50));
         this.z = z - 1;
+
+        try {
+            O = ImageIO.read(new File("O.jpg"));
+            X = ImageIO.read(new File("X.jpg"));
+        } catch (IOException ex) {
+        }
     }
 
     @Override
@@ -26,20 +41,20 @@ class GamePanel extends JPanel {
             for (int j = 0; j < 4; j++) {
                 switch (GameController.map.getSign(i, j, z)) {
                     case NONE:
-                        g.setColor(new Color(0, 255, 255));
+                        g.setColor(new Color(200, 200, 200));
+                        g.fillRect(i * 95, j * 90, 90, 85);
                         break;
                     case O:
-                        g.setColor(new Color(0, 0, 255));
+                        g.drawImage(O, i * 95, j * 90, null);
                         break;
                     case X:
-                        g.setColor(new Color(0, 255, 0));
+                        g.drawImage(X, i * 95, j * 90, null);
                         break;
                     default:
                         g.setColor(new Color(0, 0, 0));
                 }
-                g.fillRect(i * 95, j * 90, 90, 85);
+                //g.fillRect(i * 95, j * 90, 90, 85);
             }
         }
     }
-    
 }
